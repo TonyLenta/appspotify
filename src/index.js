ac3ef008d256252a8f093b56d5d0454e9c658d2a
anthony sanchez <anthony.sanchez@pucese.edu.ec>
2020-06-10 03:37:02 +0000

    Agreegando rutas para las vistas dos y tres en el index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 const app = require('./config/server'); 
+
 require('./app/routes/views')(app); 
+require('./app/routes/views2')(app);
+require('./app/routes/views3')(app);
+
 // starting the server 
 app.listen(app.get('port'), () => {
   console.log('server on port', app.get('port'));
