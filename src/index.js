395b77236d0fae9755b5b7e16341ea586981d47f
anthony sanchez <anthony.sanchez@pucese.edu.ec>
2020-06-18 05:48:17 +0000

    Agregando ruta para el home

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = require('./config/server');
 require('./app/routes/views')(app); 
 require('./app/routes/views2')(app);
 require('./app/routes/views3')(app);
-
+require('./app/routes/home')(app);
 // starting the server 
 app.listen(app.get('port'), () => {
   console.log('server on port', app.get('port'));
