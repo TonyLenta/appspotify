fd8889d51b3cb6cb87b06157fd8fa891a8b21433
anthony sanchez <anthony.sanchez@pucese.edu.ec>
2020-06-18 07:41:56 +0000

    agregando ruta de api

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ require('./app/routes/views')(app);
 require('./app/routes/views2')(app);
 require('./app/routes/views3')(app);
 require('./app/routes/home')(app);
+require('./app/api/api')(app);
+
 // starting the server 
 app.listen(app.get('port'), () => {
   console.log('server on port', app.get('port'));
